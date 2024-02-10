460c79154ff052a8993d43adae14fb1b3fc5df62
Ankit Deb <deb.ankit.work@gmail.com>
2024-02-11 02:42:49 +0530

    completed work on Navbar. made responsive, minor functionality of useState of React

--- ankit_portfolio/src/App.js
+++ ankit_portfolio/src/App.js
@@ -1,11 +1,17 @@
 
 import './App.css';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
 
 function App() {
   return (
-    <div className="App">
-      Hello Duniya
-    </div>
+    <>
+      <Header />
+      <main className='main'>
+        <Home />
+
+      </main>
+    </>
   );
 }
 
